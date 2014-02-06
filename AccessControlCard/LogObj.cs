414597dd05bc0c33a61f41d1210aac51355fce35
imcloudwu <imcloudwu@gmail.com>
2014-02-06 11:45:33 +0800

    Log補完

---
+++ AccessControlCard/LogObj.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace AccessControlCard
+{
+    class LogObj
+    {
+        public String Id, Name, Sms_phone, Card_no;
+
+        public LogObj(DataRow row)
+        {
+            this.Id = row["id"].ToString();
+            this.Name = row["name"].ToString();
+            this.Sms_phone = row["sms_phone"].ToString();
+            this.Card_no = row["card_no"].ToString();
+        }
+    }
+}
